1366aeff9aadfd712ab390818a78bdfc6bbb5c77
Tano332 <jojjo02@live.se>
2023-04-06 16:40:34 +0200

    tumblebounce

--- Architecture/Assets/Jojo Assets/Scripts/TumbleweedScript.cs
+++ Architecture/Assets/Jojo Assets/Scripts/TumbleweedScript.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class TumbleweedScript : MonoBehaviour
 {
     public Rigidbody rigidBody;
-    public float radius;
-    public int movementSpeed;
+    public float radius, movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +20,15 @@ public class TumbleweedScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(-movementSpeed, 0.0f, 0.0f);
+        if (transform.position.x > -70)
+        {
+            Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f) * movementSpeed * Time.fixedDeltaTime;
 
-        rigidBody.AddForce(movement * movementSpeed);
+            rigidBody.AddForce(movement * movementSpeed);
+        }
+        else
+        {
+            Destroy(rigidBody);
+        }
     }
 }
