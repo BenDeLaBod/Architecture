b093d80e73c6022b50bbd075ec6d8c6bff433543
BrandonMach <brandon.mach2003@gmail.com>
2023-04-20 21:35:42 +0200

    test wanted poster

--- Architecture/Assets/BrandonAssets/BrandonScripts/HealthPoints.cs
+++ Architecture/Assets/BrandonAssets/BrandonScripts/HealthPoints.cs
@@ -7,20 +7,22 @@ public class HealthPoints : MonoBehaviour
     // Start is called before the first frame update
     public int healthPoints;
 
-    [SerializeField] private IntEventSO _scoreEvent;
+    [SerializeField] private IntEventSO _hpEvent;
     [SerializeField] private int _deathMoney = 33;
 
     // Update is called once per frame
     void Update()
     {
-        healthPoints = Mathf.Clamp(healthPoints, 0, 100);
+        
+        
+
         if (healthPoints <= 0)
         {
             Debug.Log("Dead");
             Die();
             if(gameObject.tag == "EnemyTest")
             {
-                _scoreEvent.Invoke(_deathMoney);                     
+                _hpEvent.Invoke(_deathMoney);                     
             }
         }
     }
