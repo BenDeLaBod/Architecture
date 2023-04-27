a10b00f12da77719e854c4a645a5a578a7c1bdd0
BenDeLaBod <benjamin.bodin02@gmail.com>
2023-04-27 12:05:56 +0200

    boop

--- Architecture/Assets/BrandonAssets/BrandonScripts/BulletScript.cs
+++ Architecture/Assets/BrandonAssets/BrandonScripts/BulletScript.cs
@@ -7,33 +7,38 @@ public class BulletScript : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float _bulletSpeed = 10;
     float _lifeSpan = 2.5f;
+
+    Vector3 direction;
     
     void Start()
     {
-        
+        direction = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * _bulletSpeed);
+        transform.position +=  Time.deltaTime * _bulletSpeed * direction;
         _lifeSpan -= Time.deltaTime;
 
         if(_lifeSpan <= 0)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
     }
 
 
     private void OnCollisionEnter(Collision targetHit)
     {
-        if (targetHit.gameObject.CompareTag("EnemyTest"))
+        if (!targetHit.gameObject.CompareTag("Gun"))
         {
-            Destroy(this.gameObject);
-            Debug.Log("Enemy Hit");
-            targetHit.gameObject.GetComponent<HealthPoints>().TakeDamage(1);
-            
+            if (targetHit.gameObject.CompareTag("EnemyTest"))
+            {
+                Debug.Log("Enemy Hit");
+                targetHit.gameObject.GetComponent<HealthPoints>().TakeDamage(1);
+            }
+
+            Destroy(gameObject);
         }
     }
 }
