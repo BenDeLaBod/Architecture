30bc1f15a8e408d4182e3bf418a252300eb83bbf
BrandonMach <brandon.mach2003@gmail.com>
2023-05-04 09:41:24 +0200

    Betting and undo bet works

--- Architecture/Assets/BrandonAssets/BrandonScripts/HUDScripts/NPCTalk/BettingScript.cs
+++ Architecture/Assets/BrandonAssets/BrandonScripts/HUDScripts/NPCTalk/BettingScript.cs
@@ -7,39 +7,69 @@ using UnityEngine;
 public class BettingScript : MonoBehaviour
 {
 
-
     [SerializeField] private TextMeshProUGUI _bettingText;
-    [SerializeField] private int _bettingMoney;
+    [SerializeField] public double bettingMoney;
+    [SerializeField] public double lastBet;
+    Stack<int> _bettingHistory = new Stack<int>();
+
     public void Add5()
     {
-        _bettingMoney +=5;
+        //ActionBase action = new Add5Dollar(this);
+
+        //_bettingRecorder.Record(action);
+
+        bettingMoney += 5f;
         UpdateBettingText();
+        lastBet = 5;
+        _bettingHistory.Push(5);
     }
     public void Add10()
     {
-        _bettingMoney += 10;
+
+        bettingMoney += 10f;
         UpdateBettingText();
+        lastBet = 10f;
+        _bettingHistory.Push(10);
     }
     public void Add20()
     {
-        _bettingMoney += 20;
+
+        bettingMoney += 20f;
         UpdateBettingText();
+        lastBet = 20;
+        _bettingHistory.Push(20);
     }
     public void Add50()
     {
-        _bettingMoney += 50;
+        bettingMoney += 50f;
         UpdateBettingText();
+        lastBet = 50;
+        _bettingHistory.Push(50);
     }
     public void Add100()
     {
-        _bettingMoney += 100;
+
+        bettingMoney += 100f;
         UpdateBettingText();
+        lastBet = 100;
+        _bettingHistory.Push(100);
     }
 
-    private void UpdateBettingText()
+    public void UndoMoney()
     {
-        _bettingText.text =  "Your bet amount: $" +  _bettingMoney.ToString();
+        if(bettingMoney > 0)
+        {
+            bettingMoney -= _bettingHistory.Pop();
+            UpdateBettingText();
+        }
+       
     }
 
+
+    public void UpdateBettingText()
+    {
+        //_bettingMoney *= 0.5f;
+        _bettingText.text = "Your bet amount: $" +  bettingMoney.ToString();
+    }
 
 }
