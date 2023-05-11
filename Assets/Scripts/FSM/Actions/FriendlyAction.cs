cd23feded74ea3e4d8e843de2e3f04733432f829
AJervill <89905411+AJervill@users.noreply.github.com>
2023-05-11 11:29:48 +0200

    fixed alot of things

---
+++ Assets/Scripts/FSM/Actions/FriendlyAction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(menuName = "FSM/Actions/Friendly")]
+
+public class FriendlyAction : FSMAction
+{
+    public override void Execute(StateMachine stateMachine)
+    {
+        var navMeshAgent = stateMachine.GetComponent<NavMeshAgent>();
+        navMeshAgent.isStopped = true;
+    }
+}
---
+++ Assets/Scripts/FSM/IsEnemyChecker.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsEnemyChecker : MonoBehaviour
+{
+    public bool beenMadeEnemy;
+
+    public bool CheckForEnemy()
+    {
+        return beenMadeEnemy;
+    }
+}
